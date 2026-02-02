f15df2d21d184754f33f4974b341793d1c1d3a87
Yanzhou Wang <74737549+Yanzhou-Wang@users.noreply.github.com>
2026-02-02 13:34:15 +0800

    remove empty FAIL dir

--- vasp-related/screen-out-scf-not-converged-job_fr-outcar.sh
+++ vasp-related/screen-out-scf-not-converged-job_fr-outcar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 targ_file="OUTCAR"
 MAINPATH=`pwd`
-case_class=(Au-done  Pb-done  Pd-done  Pt-done  Zr-done)    #????????????
+case_class=(.)    #???????????????????? Usually it is the current directory???
 for index in ${case_class[*]}
 do
         for i in `find $MAINPATH/$index -name "$targ_file" | sed 's/\/'$targ_file'//g'`
@@ -9,7 +9,7 @@ do
                 str1=$(echo $i | awk -F"/" '{print $(NF)}')     #Au.03.MP.5  #i=/scratch/work/wangy43/withUNEP/220728_InitStrc_18elements/3vasp/Au-done/Au.03.MP.5
                 str2=$(echo $i | awk -F"/" '{print $(NF-1)}')   #Au-done
                 str=$(echo $i | sed 's/\/'$str2'\/'$str1'//g')   #/scratch/work/wangy43/withUNEP/220728_InitStrc_18elements/3vasp
-                writ_dire="$str/Fail_$str2"
+                writ_dire="$str/FAIL-SCF_$str2"
                 mkdir -p $writ_dire
 
                 dest="$i"
@@ -20,11 +20,18 @@ do
                                 echo "!!!!! $str2/$str1 NOT converged ... !!!!"
                                 mv $dest $writ_dire
                         else
-                                 echo "$str2/$str1 ok ..."
+                                 echo "$str2/$str1 : scf successfully finished ..."
                         fi
                 else
                         echo "! SOMETHING WRONG (the job fails to finish)! | $str2/$str1"
                         mv $dest $writ_dire
                 fi
-        done
+
+        	# if $writ_dire is empty, remove it
+        	if [[ -d "$writ_dire" && -z "$(ls "$writ_dire")" ]]; then
+                	rmdir "$writ_dire"
+        	fi
+
+        	done
+
 done
