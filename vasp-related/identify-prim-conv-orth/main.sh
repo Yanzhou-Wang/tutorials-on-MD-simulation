b87fbd521454d6d961debf229909812d0d2b5625
Yanzhou Wang <74737549+Yanzhou-Wang@users.noreply.github.com>
2026-02-26 19:53:02 +0800

    Update main.sh

--- vasp-related/identify-prim-conv-orth/main.sh
+++ vasp-related/identify-prim-conv-orth/main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+./py_identify-vasp-stru_2_prim-conv-orth.py --name "CONTCAR" ../260224-1_vasp-optimize-Cu-Ta-Li-Ni_simple-substance_ksp0.1-conv_frpre2 ./
 
-./py_identify-vasp-stru_2_prim-conv-orth.py CONTCAR
+#上述代码执行后直接把read_dir的目录下的CONCAR以及对应的子目录，和转化好的conv.vasp, orth.vasp, prim.vasp，一并转化到当前目录下了。 执行后的结果，就是继承了read_dir的子目录层级。
