948e18ba1c9cf4541845b098338fac3c90270357
Yanzhou Wang <74737549+Yanzhou-Wang@users.noreply.github.com>
2026-03-10 21:53:33 +0800

    Update screen-out-scf-not-converged-job_fr-outcar.sh

--- vasp-related/screen-out-scf-not-converged-job_fr-outcar.sh
+++ vasp-related/screen-out-scf-not-converged-job_fr-outcar.sh
@@ -1,37 +1,59 @@
 #!/bin/bash
-targ_file="OUTCAR"
-MAINPATH=`pwd`
-case_class=(.)    #Au-done Cu-done ...???????????????????? Usually it is the current directory???
-for index in ${case_class[*]}
+# Usage: ./script.sh  # 在当前路径下查找运行abnormal jobs, 或者normal但是scf不收敛的jobs，并把他们分别移入移当前目录的"ABNORMAL-JOB"，和FAIL-SCF-JOB目录。 并继承了他们原来的相对目录
+#
+# Assume abnormal jobs are organized as follows: 
+# ./
+# |__ id-mp-1191664-Ta5Ni  
+# |__ id-mp-1218103-Ta4Cu3Ni9  
+#      |__id-mp-1225687-CuNi
+#         |__id-mp-1191669-Ta5Ni9 
+#
+#[OUTPUTs]:		#正常结束的jobs,但是scf不收敛
+# ./
+# |__FAIL-SCF-JOB
+#    |__ id-mp-1191664-Ta5Ni  
+#    |__ id-mp-1218103-Ta4Cu3Ni9  
+#        |__id-mp-1225687-CuNi 
+#           |__id-mp-1191669-Ta5Ni9
+#
+#[OUTPUTs]:
+# ./
+# |__ABNORMAL-JOB	#非正常结束的jobs.
+#    |__ id-mp-119164-Cu5Ni  
+#    |__ id-mp-121813-Ta4Cu3  
+#        |__id-mp-122567-CuNi3 
+
+r_f="OUTCAR"
+
+for i in $(find . \( -path "./FAIL-SCF-JOB" -o -path "./ABNORMAL-JOB" \) -prune -o -name "$r_f" -print | sed 's#/'${r_f}'##') # exclude from walking through ./FAIL-SCF-JOB and ./ABNORMAL-JOB dirs.
 do
-        for i in `find $MAINPATH/$index -name "$targ_file" | sed 's/\/'$targ_file'//g'`
-        do
-                str1=$(echo $i | awk -F"/" '{print $(NF)}')     #Au.03.MP.5  #i=/scratch/work/wangy43/withUNEP/220728_InitStrc_18elements/3vasp/Au-done/Au.03.MP.5
-                str2=$(echo $i | awk -F"/" '{print $(NF-1)}')   #Au-done
-                str=$(echo $i | sed 's/\/'$str2'\/'$str1'//g')   #/scratch/work/wangy43/withUNEP/220728_InitStrc_18elements/3vasp
-                writ_dire="$str/FAIL-SCF_$str2"
-                mkdir -p $writ_dire
-
-                dest="$i"
-                if [[ `grep "General timing and accounting informations for this job" $dest/$targ_file` ]]; then
-                        NELM=$(grep -G "^[ ]*[ ]NELM[ ][ ]*" $dest/$targ_file | tail -n 1 | awk -F';' '{print $1}' |awk '{print $3}')
-                        electron_step=$(grep "^[ -][ -]*Iteration" $dest/$targ_file |tail -n 1 | awk '{print $4}'| awk -F')' '{print $1}')
-                        if [[ $electron_step -eq $NELM ]]; then
-                                echo "!!!!! $str2/$str1 NOT converged ... !!!!"
-                                mv $dest $writ_dire
-                        else
-                                 echo "$str2/$str1 : scf successfully finished ..."
-                        fi
+	job_path="$i"
+	if [[ -n $(tail -n 25  ${job_path}/$r_f | grep "General timing and accounting informations for this job:") ]]
+	then
+       		NELM=$(grep -G "^[ ]*[ ]NELM[ ][ ]*" ${job_path}/$r_f | tail -n 1 | awk -F';' '{print $1}' |awk '{print $3}')
+                electron_step=$(grep "^[ -][ -]*Iteration" ${job_path}/$r_f |tail -n 1 | awk '{print $4}'| awk -F')' '{print $1}')
+                
+		if [[ $electron_step -eq $NELM ]]; 
+		then
+                	w_job_path=$(echo $job_path | sed 's#\./#\./FAIL-SCF-JOB/#')
+	        	w_job_parent=$(dirname "$w_job_path")
+   		     	mkdir -p $w_job_parent
+        		mv $job_path $w_job_parent
+        		echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+        		echo ">>>>>>>>> FAIL-SCF-JOB:  \"$job_path\" moved into \"$w_job_parent\" <<<<<<<<<<<<<<<<<"
+        		echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
                 else
-                        echo "! SOMETHING WRONG (the job fails to finish)! | $str2/$str1"
-                        mv $dest $writ_dire
+                        echo "SCF for $job_path is successfully finished"
                 fi
+                
+	else
+		w_job_path=$(echo $job_path | sed 's#\./#\./ABNORMAL-JOB/#')
+		w_job_parent=$(dirname "$w_job_path")
+		mkdir -p $w_job_parent
+		mv $job_path $w_job_parent
+		echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+		echo ">>>>>>>>> ABNORMAL-JOB: \"$job_path\" moved into \"$w_job_parent\" <<<<<<<<<<<<<<<<<"
+		echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 
-        	# if $writ_dire is empty, remove it
-        	if [[ -d "$writ_dire" && -z "$(ls "$writ_dire")" ]]; then
-                	rmdir "$writ_dire"
-        	fi
-
+	fi
         	done
-
-done
