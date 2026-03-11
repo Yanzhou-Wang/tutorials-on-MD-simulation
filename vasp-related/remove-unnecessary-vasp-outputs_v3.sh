24bf929a5c65bc4b6dc08102895094030eadb374
Yanzhou Wang <74737549+Yanzhou-Wang@users.noreply.github.com>
2026-03-11 10:50:50 +0800

    Update remove-unnecessary-vasp-outputs_v3.sh

--- vasp-related/remove-unnecessary-vasp-outputs_v3.sh
+++ vasp-related/remove-unnecessary-vasp-outputs_v3.sh
@@ -2,19 +2,17 @@
 
 # Usage: ./script.sh  	#在当前目录下递归遍历vasp的输出文件，删除用户指定的文件
 # rm_outs=(xxx)数组中的文件名，如果以#开头，比如#vasprun.xml时，script.sh会排除对该文件的遍历和删除操作。
-:<<!
-CONTCAR
-IBZKPT
-INCAR
-KPOINTS
-OSZICAR
-OUTCAR
-POSCAR
-POTCAR
-REPORT
-vasprun.xml
-XDATCAR
-!
+#CONTCAR
+#IBZKPT
+#INCAR
+#KPOINTS
+#OSZICAR
+#OUTCAR
+#POSCAR
+#POTCAR
+#REPORT
+#vasprun.xml
+#XDATCAR
 
 rm_outs=(
 CHG
