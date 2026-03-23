1fde12ec54fa210848557b208150e0796582e6c3
Yanzhou Wang <74737549+Yanzhou-Wang@users.noreply.github.com>
2026-03-23 13:39:43 +0800

    Create launch-gpumd-job.sh

---
+++ vasp-related/launch-gpumd-job.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+cwd=`pwd`
+nep_dest="$HOME/PATH-TO-NEP-FILE"
+code="$HOME/PATH-TO-GPUMD-CODE"
+stru_dir="$cwd/DIRE-TO-STRUCTURE"
+
+is=(XX YY ZZ)                               
+for i in ${!is[*]}
+do
+	  js=(XX2 YY2 ZZ2)
+	  for j in ${!js[*]}
+    do
+        ks=(XX3 YY3 ZZ3)
+        for k in ${!ks[*]} 
+        do
+            jn="JOB-NAME"
+            mkdir -p $jn
+            cd $jn
+    
+            #model.xyz
+            stru_n="STRU-FILE"
+            cp $stru_dir/$stru_n ./model.xyz
+    
+            #run.in
+            cat > run.in << !
+XXXXXXXXXXX
+........
+XXXXXXXXXX
+!
+            #sbatch script
+			      cat > submit-gpumd-job.sbatch <<!
+YYYYYYYYY
+.........
+YYYYYYYYY
+!
+			      sbatch submit-gpumd-job.sbatch
+            cd $cwd
+        
+            echo ">>>>>>>>>>>>> ${is[i]} ${js[j]} ${ks[k]} <<<<<<<<<<<<<<<<<"
+			      sleep 1s
+		    done
+    done
+done
