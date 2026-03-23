ea8f8b3222dcdaceaef0103f8da3366e75139875
Yanzhou Wang <74737549+Yanzhou-Wang@users.noreply.github.com>
2026-03-23 11:28:58 +0800

    Update launch-vasp-job.sh

--- vasp-related/launch-vasp-job.sh
+++ vasp-related/launch-vasp-job.sh
@@ -4,11 +4,11 @@ cwd=$(pwd)
 pot_dir="$cwd/PATH-TO-POT-DIR"        # Mandatory Argument. In the "$pot_dir", you must see POTCAR files forcbly named Cu, Ta, Li, Ni etc
 stru_dir="$cwd/PATH-To-STRU-DIR"      # Mandatory Argument. $struc_dir refers where structure files for POSCAR are
 
-items1=(XX YY ZZ)                     # Mandatory Argument, which is used to map structure name or job name
-for i in ${items1[*]}
+is=(XX YY ZZ)                     # Mandatory Argument, which is used to map structure name or job name
+for i in ${is[*]}
 do
-	items2=(XX2 YY2 ZZ2)                # Mandatory Argument, which is secondary for mapping structure name or job name 
-	for j in ${items2[*]}
+	js=(XX2 YY2 ZZ2)                # Mandatory Argument, which is secondary for mapping structure name or job name 
+	for j in ${js[*]}
 	do
 		jn="interface_shift${i}_dist${j}"                    # Mandatory Argument, for building up job name
 		
@@ -21,9 +21,9 @@ do
 	        
 		#POTCAR
 		cp ${pot_dir}/* .
-    dos2unix -q POSCAR 
-    sed -n '6p' POSCAR |xargs cat  > POTCAR       # Caveat: one's POTCAR files in ${pot_dir} MUST BE NAMED BY ELEMENT SYMBOLS, like Cu, Li, Ni, Ta etc.
-    rm -f Cu  Li  Ni  Ta                          # Note: sync correspondingly
+        dos2unix -q POSCAR 
+        sed -n '6p' POSCAR |xargs cat  > POTCAR       # Caveat: one's POTCAR files in ${pot_dir} MUST BE NAMED BY ELEMENT SYMBOLS, like Cu, Li, Ni, Ta etc.
+        rm -f Cu  Li  Ni  Ta                          # Note: sync correspondingly
 
 		#KPOINTS
 		#ksp=0.2
