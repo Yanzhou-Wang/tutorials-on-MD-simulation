0ef8bbd78f0b61d8550890b2742d3f07d782b1a7
Yanzhou-Wang <yanz_wang@126.com>
2026-04-10 11:31:43 +0800

    updated just now

--- vasp-related/260227-2_gpumd-single-point-triaxial-EOS_Li_fr260227-1u1/1copy-input-struc-here.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wd="input-struc"
-mkdir $wd
-
-cp -rv ../260227-1u1_*/id-mp* ./$wd
-
-
--- vasp-related/260229-2_gpumd-single-point_uniaxial_Li_fr260229-1u1/1copy-input-struc-here.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wd="input-struc"
-mkdir $wd
-
-cp -rv ../260229-1u1_*/id-mp* ./$wd
-
-
